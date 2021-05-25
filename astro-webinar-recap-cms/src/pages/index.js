3e5acdf2c8aa16136a87756f00d25d15eee018b7
medinab <medina1brandon@gmail.com>
2021-05-25 15:48:50 -0400

    rename file structure

---
+++ astro-webinar-recap-cms/src/pages/index.js
@@ -0,0 +1,21 @@
+import React from "react"
+import './index.css'
+import { Button, AstroLogo } from "@astronomer/spectra";
+
+export default function Home() {
+  return (
+    <body>
+      <div className='top'>
+        <AstroLogo />
+      </div>
+      <div className='button'>
+        <Button
+          label="Continue to Webinar Recap CMS"
+          to="/admin/"
+          type="external"
+          gatsby
+        />
+      </div>
+    </body>
+  )
+}
